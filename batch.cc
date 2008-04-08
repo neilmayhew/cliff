dd144fe47df6dd944e8791fa6bbfd64ca5f0a500
Neil Mayhew <neil@mayhew.name>
2008-04-08 04:49:12 +0000

    Use std namespace

--- batch.cc
+++ batch.cc
@@ -31,9 +31,9 @@
  *		s*s = 1 / 2(1 + gh/v*v)
  */
 
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
 #include <unistd.h>
 
 const double g = 9.80665;
@@ -55,13 +55,13 @@ int main(int argc, char* argv[])
 {
 	int c;
 
-	while ((c = getopt(argc, argv, "ep:")) != EOF)
+	while ((c = getopt(argc, argv, "ep:")) != -1)
 	{
 		switch (c)
 		{
 		case 'p': /* Check perturbations */
 			popt++;
-			pval = atof(optarg);
+			pval = std::atof(optarg);
 			break;
 		case 'e': /* Check for algebra errors */
 			eopt++;
@@ -80,16 +80,16 @@ int main(int argc, char* argv[])
 		if (!getrange("h", &hs, &he, &hi))
 			break;
 
-		putchar('\n');
+		std::putchar('\n');
 
 		for (v = vs ; v < ve ; v += vi)
 			for (h = hs ; h < he ; h += hi)
 				compute(v, h);
 
-		putchar('\n');
+		std::putchar('\n');
 	}
 
-	exit(0);
+	return 0;
 }
 
 int getrange(char* item, double* s, double* e, double* i)
@@ -98,13 +98,13 @@ int getrange(char* item, double* s, double* e, double* i)
 	{
 		char buf[100];
 
-		fprintf(stderr, "Enter range of %s: start [end [increment]] ",
+		std::fprintf(stderr, "Enter range of %s: start [end [increment]] ",
 			item);
 
-		if (!fgets(buf, sizeof(buf), stdin))
+		if (!std::fgets(buf, sizeof(buf), stdin))
 			return 0;
 
-		switch (sscanf(buf, "%lf%lf%lf", s, e, i))
+		switch (std::sscanf(buf, "%lf%lf%lf", s, e, i))
 		{
 		case EOF:
 		case 0:
@@ -123,23 +123,23 @@ int getrange(char* item, double* s, double* e, double* i)
 			break;
 
 		default:
-			fputs("too many items specified\n", stderr);
+			std::fputs("too many items specified\n", stderr);
 			continue;
 		}
 
 		if (*s == *e)
 		{
-			fputs("start = end\n", stderr);
+			std::fputs("start = end\n", stderr);
 			continue;
 		}
 		if (*i == 0.0)
 		{
-			fputs("zero increment\n", stderr);
+			std::fputs("zero increment\n", stderr);
 			continue;
 		}
 		if (*s < *e && *i < 0.0 || *s > *e && *i > 0.0)
 		{
-			fputs("increment has wrong sign\n", stderr);
+			std::fputs("increment has wrong sign\n", stderr);
 			continue;
 		}
 
@@ -153,20 +153,20 @@ double compute(double v, double h)
 
 	temp = 0.5 / (1.0 + g * h / (v * v));
 
-	s = sqrt(temp);
-	p = asin(s);
+	s = std::sqrt(temp);
+	p = std::asin(s);
 
-	c = sqrt(1.0 - temp);
+	c = std::sqrt(1.0 - temp);
 	d = v * v * c / (g * s);
 
-	printf("v =% -7.5g  h =% -7.5g  p =% -7.5g  d =% -7.5g\n",
+	std::printf("v =% -7.5g  h =% -7.5g  p =% -7.5g  d =% -7.5g\n",
 		v, h, r2d(p), d);
 
 	if (eopt)
 	{
 		double d1 = dist(p, v, h);
-		if (fabs(d - d1) > 1.e-4)
-			printf("Error: D =% -7.5g\n", d1);
+		if (std::abs(d - d1) > 1.e-4)
+			std::printf("Error: D =% -7.5g\n", d1);
 	}
 
 	if (popt)
@@ -174,7 +174,7 @@ double compute(double v, double h)
 		double pert1 = d - dist(p * (1. + pval), v, h);
 		double pert2 = d - dist(p * (1. - pval), v, h);
 		if (pert1 < 0 || pert2 < 0)
-			printf("Not maximal: p+ =% -7.5g  p- =% -7.5g\n",
+			std::printf("Not maximal: p+ =% -7.5g  p- =% -7.5g\n",
 				pert1, pert2);
 	}
 
@@ -183,8 +183,8 @@ double compute(double v, double h)
 
 double dist(double p, double v, double h)
 {
-	double c = cos(p);
-	double s = sin(p);
+	double c = std::cos(p);
+	double s = std::sin(p);
 
-	return v * v * c / g * (s + sqrt(s * s + 2. * g * h / v / v));
+	return v * v * c / g * (s + std::sqrt(s * s + 2. * g * h / v / v));
 }
