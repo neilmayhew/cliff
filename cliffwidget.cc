94fb0c8bd529c61eaa4b6acdb96a5c48f86a137a
Neil Mayhew <neil@mayhew.name>
2009-03-14 03:04:24 +0000

    Use a white background

--- cliffwidget.cc
+++ cliffwidget.cc
@@ -5,7 +5,7 @@
  *
  *	Neil Mayhew - 2007-12-08
  *
- *	$Id: cliffwidget.cc,v 1.3 2009-03-14 02:23:10 mayhewn Exp $
+ *	$Id: cliffwidget.cc,v 1.4 2009-03-14 03:04:24 mayhewn Exp $
  */
 
 #include "cliffwidget.h"
@@ -33,7 +33,11 @@ bool CliffWidget::on_expose_event(GdkEventExpose* event)
 	double height = 50.0;        // m
 	double size_x = (double)allocation.get_width();
 	double size_y = (double)allocation.get_height();
-	
+
+    cr->rectangle(0, 0, size_x, size_y);
+    cr->set_source_rgb(1.0, 1.0, 1.0);
+    cr->fill();
+
 	render(cr, velocity, height, size_x, size_y);
 
 	return true;
