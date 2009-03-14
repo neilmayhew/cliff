ec4ebffb9e4e782bbe52de3bca32687114a14abf
Neil Mayhew <neil@mayhew.name>
2009-03-14 05:09:59 +0000

    Upgrade to handle multiple image formats

--- cliffsvg.cc
+++ cliffsvg.cc
@@ -1,25 +1,69 @@
 /*
  *	cliffsvg.cc
  *
- *	Render a cliff diagram to SVG
+ *	Render a cliff diagram to one of several image formats
  *
  *	Neil Mayhew - 2009-03-13
  *
- *	$Id: cliffimg.cc,v 1.1 2009-03-14 02:58:06 mayhewn Exp $
+ *	$Id: cliffimg.cc,v 1.2 2009-03-14 05:09:59 mayhewn Exp $
  */
 
 #include "cliffcairo.h"
 
 #include <cairomm/surface.h>
 
-int main(int argc, char const* argv[])
+#include <iostream>
+#include <string>
+#include <libgen.h>
+
+int main(int argc, char* argv[])
 {
-	Cairo::RefPtr<Cairo::SvgSurface> svg = Cairo::SvgSurface::create(
-		argv[1], 500, 500);	 
+	if (argc != 2)
+	{
+		std::cerr << "Usage: "<<basename(argv[0])<<" FILE\n";
+		return 1;
+	}
 
-	Cairo::RefPtr<Cairo::Context> cr = Cairo::Context::create(svg);
+	std::string filename(argv[1]);
 
-	render(cr, 20, 50, 500, 500);
+	int size_x = 500;
+	int size_y = 500;
+
+	double velocity = 20.0;
+	double height   = 50.0;
+
+	std::string suffix;
+	
+	if (filename.rfind('.') != filename.npos)
+		suffix = filename.substr(filename.rfind('.') + 1);
+
+	Cairo::RefPtr<Cairo::Surface> surface;
+
+	if (suffix == "pdf")
+		surface = Cairo::PdfSurface::create(filename, size_x, size_y);
+	else if (suffix == "svg")
+		surface = Cairo::SvgSurface::create(filename, size_x, size_y);
+	else if (suffix == "png")
+		surface = Cairo::ImageSurface::create(Cairo::FORMAT_RGB24, size_x, size_y);
+	else
+	{
+		std::cerr << "Unknown or missing file suffix: " << filename << "\n";
+		return 2;
+	}
+
+	Cairo::RefPtr<Cairo::Context> context = Cairo::Context::create(surface);
+
+	if (suffix == "png")
+	{
+	    context->rectangle(0, 0, size_x, size_y);
+	    context->set_source_rgb(1.0, 1.0, 1.0);
+	    context->fill();
+	}
+
+	render(context, velocity, height, size_x, size_y);
+
+	if (suffix == "png")
+		surface->write_to_png(filename);
 
 	return 0;
 }
